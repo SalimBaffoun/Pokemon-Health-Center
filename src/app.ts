8b30e1a109546d9adbfbabed4497d8f78ba75f98
SALIMARGOUB <salimargoub@gmail.com>
2023-05-18 12:00:38 +0200

    creation class pokemon

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+interface pokemon{
+    name:string;
+    type:string;
+    hp:number;
+    
+
+}
+
+class Pokemon{
+    name:string;
+    type:string;
+    hp:number;
+
+    constructor(name:string,type:string,hp:number){
+        this.name=name;
+        this.type=type;
+        this.hp=hp;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
