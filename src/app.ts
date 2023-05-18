be3b076f13d64b86388f4725dcc98d5b9441b595
SALIMARGOUB <salimargoub@gmail.com>
2023-05-18 16:05:10 +0200

    MAj des class

--- src/app.ts
+++ src/app.ts
@@ -1,80 +1,13 @@
-class Pokemon  {
+import { Pokemon } from "./pokemon.js";
+import { recoveryMachine } from "./recoveryMachine";
 
-     name: string;
-     private hp: number;
-     
-     constructor(name: string, hp: number) {
-         this.name = name;
-         this.hp = Math.max(Math.min(hp,100),0);
- }
-     
-     public getHp(): number {
-         return this.hp;
-     }
- 
-     public setHp(hp: number) {
-         this.hp = Math.max(Math.min(hp,100),0);
-     }
- 
-     levelHealth(): void {
-                 
-         console.log(`${this.name} a ${this.hp} points de vie`);
-     }
- 
- 
- }
+let pikachu = new Pokemon("Pikachu", 100,);
+let bulbizarre = new Pokemon("Bulbizarre", 100,);
 
- class recoveryMachine {
- 
- 
-         recovery(pokemon : Pokemon): void {
-            pokemon.setHp(100);     
-                //  console.log(`${pokemon.name} a ${pokemon.getHp} points de vie`);
-             }
- 
-         }
- 
- const pikachu = new Pokemon('Pikachu', 50);
- 
-pikachu.levelHealth();
+pikachu.setHp(50);
+bulbizarre.setHp(0);
 
-const recoveryMachine1 = new recoveryMachine();
-recoveryMachine1.recovery(pikachu);
-pikachu.levelHealth();
-// // const bulbizarre = new Pokemon('Bul bizarre', 100);
-// //  const salameche = new Pokemon('Salameche', 100);
-// //  const carapuce = new Pokemon('Carapuce', 100);
- 
- 
- 
- 
- 
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+pikachu.checkHealth();
+bulbizarre.checkHealth();
 
 
