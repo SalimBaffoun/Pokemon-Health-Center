4f21849289ca11410b556acf38dea86ce4e45dde
SALIMARGOUB <salimargoub@gmail.com>
2023-05-18 15:26:52 +0200

    creation class

---
+++ src/pokemon.ts
@@ -0,0 +1,38 @@
+export class Pokemon  {
+
+    public name: string;
+
+    public hp: number;
+
+    
+    constructor(name: string, hp: number) {
+        this.name = name;
+        this.hp = Math.min(hp,0);
+        this.hp = Math.max(hp,100);
+    }
+
+    setHp(hp: number): void {
+        this.hp = hp;
+    }
+
+    getHp(): number {
+        return this.hp;
+    }
+    
+    levelHealth(): void {
+                
+        console.log(`${this.name} a ${this.hp} points de vie`);
+    }
+    
+
+    checkHealth(): void {
+        if (this.hp <= 0) {
+            console.log(`${this.name} est KO`);
+        } else if (this.hp <= 50) {
+            console.log(`${this.name} est blessé`);
+        } else {
+            console.log(`${this.name} est en pleine forme`);
+        }
+    } 
+}
+
