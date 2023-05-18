cb53c111f9b52c7d059104d2ecbe552fbc87df5e
Salim Baffoun <salim.baffoun@gmail.com>
2023-05-18 15:21:23 +0200

    modif Structure html

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,56 @@
-interface pokemon{
-    name:string;
-    type:string;
-    hp:number;
-    
+class Pokemon  {
 
-}
+     name: string;
+     private hp: number;
+     
+     constructor(name: string, hp: number) {
+         this.name = name;
+         this.hp = Math.max(Math.min(hp,100),0);
+ }
+     
+     public getHp(): number {
+         return this.hp;
+     }
+ 
+     public setHp(hp: number) {
+         this.hp = Math.max(Math.min(hp,100),0);
+     }
+ 
+     levelHealth(): void {
+                 
+         console.log(`${this.name} a ${this.hp} points de vie`);
+     }
+ 
+ 
+ }
 
-class Pokemon{
-    name:string;
-    type:string;
-    hp:number;
+ class recoveryMachine {
+ 
+ 
+         recovery(pokemon : Pokemon): void {
+            pokemon.setHp(100);     
+                //  console.log(`${pokemon.name} a ${pokemon.getHp} points de vie`);
+             }
+ 
+         }
+ 
+ const pikachu = new Pokemon('Pikachu', 50);
+ 
+pikachu.levelHealth();
 
-    constructor(name:string,type:string,hp:number){
-        this.name=name;
-        this.type=type;
-        this.hp=hp;
-    }
-}
-
+const recoveryMachine1 = new recoveryMachine();
+recoveryMachine1.recovery(pikachu);
+pikachu.levelHealth();
+// // const bulbizarre = new Pokemon('Bul bizarre', 100);
+// //  const salameche = new Pokemon('Salameche', 100);
+// //  const carapuce = new Pokemon('Carapuce', 100);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
 
