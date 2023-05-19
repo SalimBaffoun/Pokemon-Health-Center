f2458b84efd4929917d13b7a232378a2bd75ed5e
SALIMARGOUB <salimargoub@gmail.com>
2023-05-19 12:53:49 +0200

    modication fond ecran et voice

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,21 @@ const hpBarElt = document.querySelector(".hp-bar");
 const hpValueElts = document.querySelectorAll(".hp-value");
 const hpBarElts = document.querySelectorAll(".progress-bar-inner");
 
+const bulbiSound  = new Audio('Pokemon-Health-Center/assets/sound/Cri_1_x_0001.ogg');
+const herbSound  = new Audio('Pokemon-Health-Center/assets/sound/Cri_0002_EB.ogg');
+const florSound  = new Audio('Pokemon-Health-Center/assets/sound/Cri_0003_EB.ogg');
+const salamecheSound  = new Audio('Pokemon-Health-Center/assets/sound/Cri_0004_EB.ogg');
+const reptincelSound  = new Audio('Pokemon-Health-Center/assets/sound/Cri_0005_EB.ogg');
+const dracaufeuSound  = new Audio('Pokemon-Health-Center/assets/sound/Cri_6_x_0001.ogg');
+
+
 const pokemons = [
-    new Pokemon("Bulbizarre", 0),
-    new Pokemon("Herbizarre", 90),
-    new Pokemon("Florizarre", 40),
-    new Pokemon("salameche", 0),
-    new Pokemon("reptincel", 10),
-    new Pokemon("dracaufeu", 100),
+    new Pokemon("Bulbizarre", 0,'bulbiSound'),
+    new Pokemon("Herbizarre", 90,'herbSound'),
+    new Pokemon("Florizarre", 40,'florSound'),
+    new Pokemon("salameche", 10,'salamecheSound'),
+    new Pokemon("reptincel", 10,'reptincelSound'),
+    new Pokemon("dracaufeu", 5,'dracaufeuSound'),
 
     
 ];
