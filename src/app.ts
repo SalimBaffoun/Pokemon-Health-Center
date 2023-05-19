b5f662ba5682232c1b7c863b33bcdf2bfea4f2aa
SALIMARGOUB <salimargoub@gmail.com>
2023-05-19 17:01:50 +0200

    button all ok

--- src/app.ts
+++ src/app.ts
@@ -39,15 +39,31 @@ for (let i = 0; i < pokemons.length; i++) {
 
 const Machine = new recoveryMachine();
 
-const recoveryOneBtns = document.querySelectorAll(".button");
-for (let i = 0; i < recoveryOneBtns.length; i++) {
-  const recoveryOneBtn = recoveryOneBtns[i];
-  recoveryOneBtn.addEventListener("click", () => {
-    Machine.recoveryOne(pokemons, i);
-    hpValueElts[i].innerHTML = `${pokemons[i].getHp()}`;
-    pokemons[i].updateHealthBar();
-  });
+// const recoveryOneBtns = document.querySelectorAll(".button");
+// for (let i = 0; i < recoveryOneBtns.length; i++) {
+//   const recoveryOneBtn = recoveryOneBtns[i];
+//   recoveryOneBtn.addEventListener("click", () => {
+//     Machine.recoveryOne(pokemons, i);
+//     hpValueElts[i].innerHTML = `${pokemons[i].getHp()}`;
+//     pokemons[i].updateHealthBar();
+//   });
+// }
+
+const recoveryAllBtn = document.querySelector(".buttonAll") as HTMLButtonElement;
+
+recoveryAllBtn.addEventListener("click", () => {
+  Machine.recoveryAll(pokemons);
+
+  for (let i = 0; i < pokemons.length; i++) {
+    const pokemon = pokemons[i];
+    hpValueElts[i].innerHTML = `${pokemon.getHp()}`;
+    pokemon.updateHealthBar();
+  }
 }
+);
+
+console.log(recoveryAllBtn);
+
 
 
 
